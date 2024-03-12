f5080a63d85a19621cdfd8c3da212adcca79200c
Lucino772 <lucapalmi772@gmail.com>
2024-03-12 21:49:50 +0100

    feat: added events emitter (#3)
    * feat: added EventEmitter class
    
    * feat: added ProcessLog event

--- src/envelop/types.py
+++ src/envelop/types.py
@@ -1,5 +1,5 @@
 import asyncio
-from typing import Coroutine, Protocol
+from typing import Any, Coroutine, Mapping, Protocol
 
 
 class ProcessWriter(Protocol):
@@ -10,3 +10,11 @@ class ProcessStopper(Protocol):
     def stop(
         self, writter: ProcessWriter, process: asyncio.subprocess.Process
     ) -> Coroutine[None, None, None]: ...
+
+
+class Event(Protocol):
+    def get_uid(self) -> str: ...
+
+    def get_name(self) -> str: ...
+
+    def get_data(self) -> Mapping[str, Any]: ...

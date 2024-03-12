0d2a275e230c806b6e20258276c7df3194ae7b93
Lucino772 <lucapalmi772@gmail.com>
2024-03-12 21:03:35 +0100

    feat: added EventEmitter class

---
+++ src/envelop/emitter.py
@@ -0,0 +1,22 @@
+import asyncio
+from typing import AsyncIterator, List
+
+from envelop.consumer import AsyncConsumer
+from envelop.types import Event
+
+
+class EventEmitter:
+    def __init__(self):
+        self.__consumers: List[AsyncConsumer[Event]] = []
+
+    async def emit(self, event: Event) -> None:
+        await asyncio.gather(*[consumer.put(event) for consumer in self.__consumers])
+
+    def __aiter__(self) -> AsyncIterator[Event]:
+        consumer = AsyncConsumer()
+        self.__consumers.append(consumer)
+        return consumer
+
+    def shutdown(self) -> None:
+        for consumer in self.__consumers:
+            consumer.dispose()

d60383072e10019f580eb60abc4b10f50e6f4bed
Lucino772 <lucapalmi772@gmail.com>
2024-03-13 13:04:26 +0100

    feat: added types for builder and servicer

--- src/envelop/types.py
+++ src/envelop/types.py
@@ -1,5 +1,7 @@
 import asyncio
-from typing import Any, Coroutine, Mapping, Protocol
+from typing import Any, Coroutine, Mapping, Protocol, Sequence
+
+import grpc
 
 
 class ProcessWriter(Protocol):
@@ -18,3 +20,21 @@ class Event(Protocol):
     def get_name(self) -> str: ...
 
     def get_data(self) -> Mapping[str, Any]: ...
+
+
+class SupportsGenericRpcHandlers(Protocol):
+    def add_generic_rpc_handlers(
+        self, generic_rpc_handlers: Sequence[grpc.GenericRpcHandler]
+    ) -> None: ...
+
+
+class Servicer(Protocol):
+    def add_rpc_handlers(self, server: SupportsGenericRpcHandlers) -> None: ...
+
+
+class Builder(Protocol):
+    def add_service(self, service: Servicer) -> None: ...
+
+
+class Module(Protocol):
+    def register(self, builder: Builder) -> Builder: ...

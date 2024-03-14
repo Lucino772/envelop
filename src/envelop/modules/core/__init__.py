847b605cf150851552f2665c465d46a8649bc6d6
Lucino772 <lucapalmi772@gmail.com>
2024-03-14 22:18:26 +0100

    feat: added register method for envelop.core module

---
+++ src/envelop/modules/core/__init__.py
@@ -0,0 +1,8 @@
+from envelop.modules.core.services import ProcessService, SystemService
+from envelop.types import Builder, Context
+
+
+def register(builder: Builder, context: Context, config: dict) -> Builder:
+    return builder.add_service(ProcessService(context)).add_service(
+        SystemService(context)
+    )

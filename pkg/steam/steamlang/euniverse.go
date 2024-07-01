c6491efeeac830912cd7fc75c22cdf7f4ead6b7c
Lucino772 <lucapalmi772@gmail.com>
2024-07-01 14:27:55 +0200

    feat: added enumes for Universe and AccountType

---
+++ pkg/steam/steamlang/euniverse.go
@@ -0,0 +1,11 @@
+package steamlang
+
+type EUniverse int32
+
+const (
+	EUniverse_Invalid  EUniverse = 0
+	EUniverse_Public   EUniverse = 1
+	EUniverse_Beta     EUniverse = 2
+	EUniverse_Internal EUniverse = 3
+	EUniverse_Dev      EUniverse = 4
+)

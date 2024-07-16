0c5add7f23f7ac7262bd9930296a27cf7263570b
Lucino772 <lucapalmi772@gmail.com>
2024-07-16 19:01:18 +0200

    feat: added utility to uncompress gzip data

--- pkg/steam/utils.go
+++ pkg/steam/utils.go
@@ -1,6 +1,11 @@
 package steam
 
-import "regexp"
+import (
+	"bytes"
+	"compress/gzip"
+	"io"
+	"regexp"
+)
 
 func BitSet64_Set(b *uint64, v uint64, offset uint32, mask uint64) {
 	*b = (*b & ^(mask << uint16(offset))) | ((v & mask) << uint16(offset))
@@ -8,6 +13,14 @@ func BitSet64_Set(b *uint64, v uint64, offset uint32, mask uint64) {
 
 func BitSet64_Get(b *uint64, offset uint32, mask uint64) uint64 {
 	return (*b >> uint16(offset)) & mask
+}
+
+func UncompressGzip(data []byte) ([]byte, error) {
+	rd, err := gzip.NewReader(bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	return io.ReadAll(rd)
 }
 
 func processRegexGroups(regex *regexp.Regexp, matches []string) map[string]string {

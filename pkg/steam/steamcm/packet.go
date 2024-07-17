0721d9887bbfe48b097939dfbbfec8fd6f2dd076
Lucino772 <lucapalmi772@gmail.com>
2024-07-17 15:16:40 +0200

    feat: added packet encoders and decoders

--- pkg/steam/steamcm/packet.go
+++ pkg/steam/steamcm/packet.go
@@ -8,40 +8,13 @@ import (
 	"math"
 
 	"github.com/Lucino772/envelop/pkg/steam/steamlang"
+	"github.com/Lucino772/envelop/pkg/steam/steampb"
 	"google.golang.org/protobuf/proto"
 )
 
 type Packet struct {
-	Header PacketHeader
+	header PacketHeader
 	buf    *bytes.Buffer
-}
-
-func NewPacket(emsg steamlang.EMsg) *Packet {
-	return &Packet{
-		Header: &StdHeader{
-			MsgType:     emsg,
-			TargetJobId: math.MaxUint64,
-			SourceJobId: math.MaxUint64,
-		},
-	}
-}
-
-func NewExtPacket(emsg steamlang.EMsg) *Packet {
-	return &Packet{
-		Header: &ExtHeader{
-			MsgType:     emsg,
-			TargetJobId: math.MaxUint64,
-			SourceJobId: math.MaxUint64,
-		},
-	}
-}
-
-func NewProtoPacket(emsg steamlang.EMsg) *Packet {
-	return &Packet{
-		Header: &ProtoHeader{
-			MsgType: emsg,
-		},
-	}
 }
 
 func ParsePacket(data []byte) (*Packet, error) {
@@ -55,17 +28,17 @@ func ParsePacket(data []byte) (*Packet, error) {
 	case steamlang.EMsg_ChannelEncryptRequest,
 		steamlang.EMsg_ChannelEncryptResponse,
 		steamlang.EMsg_ChannelEncryptResult:
-		pkt.Header = new(StdHeader)
+		pkt.header = new(StdHeader)
 	default:
 		if IsProtobuf(uint32(emsg)) {
-			pkt.Header = new(ProtoHeader)
+			pkt.header = new(ProtoHeader)
 		} else {
-			pkt.Header = new(ExtHeader)
+			pkt.header = new(ExtHeader)
 		}
 	}
 
 	buf := bytes.NewBuffer(data)
-	if _, err := pkt.Header.ReadFrom(buf); err != nil {
+	if _, err := pkt.header.ReadFrom(buf); err != nil {
 		return nil, err
 	}
 	pkt.buf = buf
@@ -73,51 +46,120 @@ func ParsePacket(data []byte) (*Packet, error) {
 }
 
 func (p *Packet) MsgType() steamlang.EMsg {
-	return p.Header.GetMsgType()
+	return p.header.GetMsgType()
 }
 
-func (p *Packet) UnmarshalBody(body io.ReaderFrom) ([]byte, error) {
-	_, err := body.ReadFrom(p.buf)
-	if err != nil {
-		return nil, err
-	}
-	return io.ReadAll(p.buf)
-}
-
-func (p *Packet) UnmarshalProtoBody(body proto.Message) error {
-	data, err := io.ReadAll(p.buf)
-	if err != nil {
-		return err
-	}
-	return proto.Unmarshal(data, body)
-}
-
-func (p *Packet) MarshalBody(body io.WriterTo) error {
-	p.buf = bytes.NewBuffer([]byte{})
-	_, err := body.WriteTo(p.buf)
-	return err
-}
-
-func (p *Packet) MarshalProtoBody(body proto.Message) error {
-	data, err := proto.Marshal(body)
-	if err != nil {
-		return err
-	}
-	p.buf = bytes.NewBuffer(data)
-	return err
-}
-
-func (p *Packet) Write(data []byte) (int, error) {
-	return p.buf.Write(data)
+func (p *Packet) Header() PacketHeader {
+	return p.header
 }
 
 func (p *Packet) Bytes() []byte {
 	var buf bytes.Buffer
-	if _, err := p.Header.WriteTo(&buf); err != nil {
+	if _, err := p.header.WriteTo(&buf); err != nil {
 		return nil
 	}
 	if _, err := p.buf.WriteTo(&buf); err != nil {
 		return nil
 	}
 	return buf.Bytes()
+}
+
+type PacketDecoder[Body_T io.ReaderFrom] struct {
+	Body    Body_T
+	Payload []byte
+}
+
+func (decoder *PacketDecoder[T]) Decode(packet *Packet) error {
+	_, err := decoder.Body.ReadFrom(packet.buf)
+	if err != nil {
+		return err
+	}
+	data, err := io.ReadAll(packet.buf)
+	if err != nil {
+		return err
+	}
+	decoder.Payload = data
+	return nil
+}
+
+type ProtoPacketDecoder[Body_T proto.Message] struct {
+	Body Body_T
+}
+
+func (decoder *ProtoPacketDecoder[T]) Decode(packet *Packet) error {
+	data, err := io.ReadAll(packet.buf)
+	if err != nil {
+		return err
+	}
+	return proto.Unmarshal(data, decoder.Body)
+}
+
+type PacketEncoder struct {
+	Header PacketHeader
+	Body   any
+	Data   *bytes.Buffer
+}
+
+func NewPacketEncoder(emsg steamlang.EMsg) *PacketEncoder {
+	return &PacketEncoder{
+		Header: &StdHeader{
+			MsgType:     emsg,
+			TargetJobId: math.MaxUint64,
+			SourceJobId: math.MaxUint64,
+		},
+		Body: nil,
+		Data: bytes.NewBuffer([]byte{}),
+	}
+}
+
+func NewExtPacketEncoder(emsg steamlang.EMsg) *PacketEncoder {
+	return &PacketEncoder{
+		Header: &ExtHeader{
+			MsgType:     emsg,
+			TargetJobId: math.MaxUint64,
+			SourceJobId: math.MaxUint64,
+		},
+		Body: nil,
+		Data: bytes.NewBuffer([]byte{}),
+	}
+}
+
+func NewProtoPacketEncoder(emsg steamlang.EMsg) *PacketEncoder {
+	return &PacketEncoder{
+		Header: &ProtoHeader{
+			MsgType: emsg,
+			Proto:   new(steampb.CMsgProtoBufHeader),
+		},
+		Body: nil,
+		Data: nil,
+	}
+}
+
+func (encoder *PacketEncoder) Encode() (*Packet, error) {
+	if encoder.Body == nil {
+		return nil, errors.New("missing packet body")
+	}
+
+	var pkt Packet
+	pkt.header = encoder.Header
+	if body, ok := encoder.Body.(proto.Message); ok {
+		data, err := proto.Marshal(body)
+		if err != nil {
+			return nil, err
+		}
+		pkt.buf = bytes.NewBuffer(data)
+	} else if body, ok := encoder.Body.(io.WriterTo); ok {
+		pkt.buf = bytes.NewBuffer([]byte{})
+		if _, err := body.WriteTo(pkt.buf); err != nil {
+			return nil, err
+		}
+		if encoder.Data != nil {
+			if _, err := encoder.Data.WriteTo(pkt.buf); err != nil {
+				return nil, err
+			}
+		}
+	} else {
+		return nil, errors.New("incomptabile packet body")
+	}
+	return &pkt, nil
 }

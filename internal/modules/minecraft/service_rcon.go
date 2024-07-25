9b69c82e3292896c00ad08ef11f99b1df31a70a8
Lucino772 <lucapalmi772@gmail.com>
2024-07-26 01:22:27 +0200

    refactor: moved rcon protocol implementation to pkg/rcon/

--- internal/modules/minecraft/service_rcon.go
+++ internal/modules/minecraft/service_rcon.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	pb "github.com/Lucino772/envelop/pkg/protobufs"
+	"github.com/Lucino772/envelop/pkg/rcon"
 	"google.golang.org/grpc"
 )
 
@@ -17,7 +18,7 @@ func NewMinecraftRconService() *minecraftRconService {
 
 func (service *minecraftRconService) SendCommand(ctx context.Context, req *pb.RconCommand) (*pb.RconResponse, error) {
 	// TODO: Check if Rcon is enabled
-	resp, err := RconSend(ctx, "localhost", 25575, "password", req.Value)
+	resp, err := rcon.Send(ctx, "localhost", 25575, "password", req.Value)
 	if err != nil {
 		return nil, err
 	}
